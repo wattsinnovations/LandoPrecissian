f4224499f4a5b27da66c42e53032a53ee0ca336f
Alessandro Simovic <alessandro@auterion.com>
2022-03-28 18:05:21 +0200

    Send pose/quaternion from Arucotag library over mavlink
    We will have to readdress how we are sending position
    and orientation from the tag over mavlink. The current
    "simulator pipeline" with sending position as the
    angular offset, but then including the orientation is
    not supported by standard PX4.

--- src/Mavlink/Mavlink.cpp
+++ src/Mavlink/Mavlink.cpp
@@ -22,12 +22,21 @@ void Mavlink::handle_message(const mavlink_message_t& message)
 	}
 }
 
-void Mavlink::send_landing_target(float angle_x, float angle_y)
+void Mavlink::send_landing_target(float angle_x, float angle_y, float q_x, float q_y, float q_z, float q_w)
 {
 	mavlink_message_t message;
 	mavlink_landing_target_t landing_target = {};
+
+	// X-axis / Y-axis angular offset of the target from the center of the image
 	landing_target.angle_x = angle_x;
 	landing_target.angle_y = angle_y;
+
+	// Quaternion of landing target orientation (w, x, y, z order, zero-rotation is 1, 0, 0, 0)
+	landing_target.q[0] = q_w;
+	landing_target.q[1] = q_x;
+	landing_target.q[2] = q_y;
+	landing_target.q[3] = q_z;
+
 
 	mavlink_msg_landing_target_encode(AUTOPILOT_SYS_ID, LANDO_COMPONENT_ID, &message, &landing_target);
 
